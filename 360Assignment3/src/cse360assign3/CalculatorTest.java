561bf71867af0219d02815dd2376e3c1b50a3e6f
Phillip Barton <pjbarto1@asu.edu>
2016-02-22 01:48:28 -0700

    Added JUnit testing for getHistory, comments

--- 360Assignment3/src/cse360assign3/CalculatorTest.java
+++ 360Assignment3/src/cse360assign3/CalculatorTest.java
@@ -10,28 +10,28 @@ public class CalculatorTest {
 	
 	@Test
 	public void testCalculator() {
-		assertNotNull(dum);
+		assertNotNull(dum);					//Asserts object is not null
 	}	
 	
 	@Test
 	public void testGetTotal() {
-		assertEquals(dum.getTotal(), 0);
+		assertEquals(dum.getTotal(), 0);	//Asserts object total initialized to 0
 	}	
 	
 	@Test
 	public void testAdd() {
 		dum.add(3);
-		assertEquals(dum.getTotal(), 3);
+		assertEquals(dum.getTotal(), 3);	//Asserts value takes addition command
 	}	
 	
 	@Test
 	public void testSubtract() {
 		dum.subtract(3);
-		assertEquals(dum.getTotal(), -3);
+		assertEquals(dum.getTotal(), -3);	//Asserts value takes subtraction command
 	}	
 	
 	@Test
-	public void testMultiply() {
+	public void testMultiply() {			//Asserts value takes multiply command
 		dum.multiply(3);
 		assertEquals(dum.getTotal(), 0);
 	}
@@ -40,36 +40,45 @@ public class CalculatorTest {
 	public void testMultiply2() {
 		dum.add(1);
 		dum.multiply(3);
-		assertEquals(dum.getTotal(), 3);
+		assertEquals(dum.getTotal(), 3);	//Asserts value takes alternate multiply command
 	}	
 	
 	@Test
 	public void testDivide() {
 		dum.divide(3);
-		assertEquals(dum.getTotal(), 0);
+		assertEquals(dum.getTotal(), 0);	//Asserts value takes divide command
 	}
 	
 	@Test
 	public void testDivide2() {
 		dum.add(3);
 		dum.divide(3);
-		assertEquals(dum.getTotal(), 1);
+		assertEquals(dum.getTotal(), 1);	//Asserts value takes alternate divide command
 	}
 	
 	@Test
 	public void testDivide3() {
 		dum.add(3);
 		dum.divide(0);
-		assertEquals(dum.getTotal(), 0);
+		assertEquals(dum.getTotal(), 0);	//Asserts value account for division by 0 "properly"
 	}
 	
 	@Test
 	public void testGetHistory() {
-		dum.add (4);
-		dum.subtract (2);
-		dum.multiply (2);
+		dum.add(4);
+		dum.subtract(2);
+		dum.multiply(2);
 		dum.add(5);
-		assertEquals(dum.getHistory(), "0 + 4 - 2 * 2 + 5");
+		assertEquals(dum.getHistory(), "0 + 4 - 2 * 2 + 5");	//Asserts history string keeps proper track of operations performed
+	}
+	
+	public void testGetHistory2() {
+		dum.add(4);
+		dum.subtract(2);
+		dum.multiply(2);
+		dum.add(5);
+		dum.divide(0);
+		assertEquals(dum.getHistory(), "0 + 4 - 2 * 2 + 5 / 0");	//Asserts history string keeps proper track of alternate operations performed
 	}
 
 }
